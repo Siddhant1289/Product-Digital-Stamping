93ff68cf49131b13f53dacf714ab53790f452bdf
Siddhant Verma <karanspd1289@gmail.com>
2022-07-27 18:47:03 +0530

    component problem resolved

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 class Login extends Component {
+  fun = () => {
+    alert("Welcome");
+  };
   render() {
     return (
       <div>
         Welcome
         <div>Hello</div>
+        <div>AAA</div>
+        <button onClick={this.fun}>Click Here</button>
       </div>
     );
   }
