8b85dd146d7a35e7f463e795dc4cbb647eecac9e
Siddhant Verma <karanspd1289@gmail.com>
2022-07-29 11:23:24 +0530

    creating login

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,86 @@
 import React, { Component } from "react";
+import "./Login.css";
 class Login extends Component {
-  fun = () => {
-    alert("Welcome");
-  };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
-      <div>
-        Welcome
-        <div>Hello</div>
-        <div>AAA</div>
-        <button onClick={this.fun}>Click Here</button>
+      <div className="body">
+        {/* ... */}
+        {/* Here Is The Login Side */}
+        <div className="login-container">
+          <div className="header">LogIn</div>
+          <div className="content">
+            <div className="form">
+              <div className="form-group">
+                <label>UserID</label>
+                <input
+                  type="text"
+                  name="UserID"
+                  className="form-group"
+                  placeholder="UserID"
+                />
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-group"
+                  name="password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div className="footer">
+              <button type="button" className="btn">
+                LogIn
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* ... */}
+        {/* Here Is The Regster Side */}
+        <div className="register-container">
+          <div className="header">Register</div>
+          <div className="content">
+            <div className="form">
+              <div className="form-group">
+                <label>UserID</label>
+                <input
+                  type="text"
+                  name="UserID"
+                  className="form-group"
+                  placeholder="UserID"
+                />
+              </div>
+              <div className="form-group">
+                <label>E-Mail</label>
+                <input
+                  type="email"
+                  name="E-Mail"
+                  className="form-group"
+                  placeholder="E-Mail"
+                />
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="form-group"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div className="footer">
+              <button type="button" className="btn">
+                LogIn
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
