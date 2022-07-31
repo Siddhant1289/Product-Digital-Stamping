59074bdd3403685cf48cc491b51561ff20c99b66
Siddhant Verma <karanspd1289@gmail.com>
2022-07-31 22:18:26 +0530

    created login Page

--- src/Login.js
+++ src/Login.js
@@ -3,81 +3,85 @@ import "./Login.css";
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { card: "" };
   }
+  openLogin = () => {};
+
+  openRegister = () => {};
   render() {
     return (
-      <div className="body">
-        {/* ... */}
-        {/* Here Is The Login Side */}
-        <div className="login-container">
-          <div className="header">LogIn</div>
-          <div className="content">
-            <div className="form">
-              <div className="form-group">
-                <label>UserID</label>
-                <input
-                  type="text"
-                  name="UserID"
-                  className="form-group"
-                  placeholder="UserID"
-                />
+      <div className="container">
+        <div className="left">
+          <div className="card">
+            <div className="inner-box" id="card">
+              <div className="card-front">
+                <h2>LOGIN</h2>
+                <form>
+                  <input
+                    type="text"
+                    className="input-box"
+                    placeholder="Enter UserID"
+                    required
+                  />
+                  <input
+                    type="password"
+                    className="input-box"
+                    placeholder="Password"
+                    required
+                  />
+                  <button type="submit" className="submit-btn">
+                    Submit
+                  </button>
+                  <input type="checkbox" />
+                  <span>Remember Me</span>
+                </form>
+                <button
+                  type="button"
+                  className="btn"
+                  onClick={this.openRegister}
+                >
+                  I'm New Here
+                </button>
+                <a href="">Forgot Password?</a>
               </div>
-              <div className="form-group">
-                <label>Password</label>
-                <input
-                  type="password"
-                  className="form-group"
-                  name="password"
-                  placeholder="Password"
-                />
-              </div>
-            </div>
-            <div className="footer">
-              <button type="button" className="btn">
-                LogIn
-              </button>
             </div>
           </div>
         </div>
-        {/* ... */}
-        {/* Here Is The Regster Side */}
-        <div className="register-container">
-          <div className="header">Register</div>
-          <div className="content">
-            <div className="form">
-              <div className="form-group">
-                <label>UserID</label>
-                <input
-                  type="text"
-                  name="UserID"
-                  className="form-group"
-                  placeholder="UserID"
-                />
+        <div className="right">
+          <div className="card">
+            <div className="inner-box" id="card">
+              <div className="card-back">
+                <h2>REGISTER</h2>
+                <form>
+                  <input
+                    type="text"
+                    className="input-box"
+                    placeholder="Your Name"
+                    required
+                  />
+                  <input
+                    type="text"
+                    className="input-box"
+                    placeholder="Enter UserID"
+                    required
+                  />
+                  <input
+                    type="password"
+                    className="input-box"
+                    placeholder="Password"
+                    required
+                  />
+                  <button type="submit" className="submit-btn">
+                    Submit
+                  </button>
+                  <input type="checkbox" />
+                  <span>Remember Me</span>
+                </form>
+                <button type="button" className="btn" onClick={this.openLogin}>
+                  I Have An Account
+                </button>
+                <a href="">Forgot Password?</a>
               </div>
-              <div className="form-group">
-                <label>E-Mail</label>
-                <input
-                  type="email"
-                  name="E-Mail"
-                  className="form-group"
-                  placeholder="E-Mail"
-                />
-              </div>
-              <div className="form-group">
-                <label>Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  className="form-group"
-                  placeholder="Password"
-                />
-              </div>
-            </div>
-            <div className="footer">
-              <button type="button" className="btn">
-                LogIn
-              </button>
             </div>
           </div>
         </div>
