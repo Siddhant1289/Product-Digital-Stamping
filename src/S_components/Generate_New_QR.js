104e502575dd9b7fb14691afc90df37e54288166
Siddhant Verma <karanspd1289@gmail.com>
2022-09-04 12:51:37 +0530

    Finalized The Project

--- src/S_components/Generate_New_QR.js
+++ src/S_components/Generate_New_QR.js
@@ -1,40 +1,126 @@
-import React from "react";
-import "./Navbar.css";
-export const Generate_New_QR = () => {
-  return (
-    <div className="container">
-      <div className="main">
-        <div className="card">
-          <div className="inner-box">
-            <div>
-              <h2>
-                <u>Generate New QR</u>
-              </h2>
-              <form>
+import axios from "axios";
+import { Component } from "react";
+import QRCode from "react-qr-code";
+
+class Generate_New_QR extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      shopid: "",
+      model: "",
+      pdate: "",
+      wdate: "",
+      result: "",
+      test: "",
+    };
+    this.handleChangeFields = this.handleChangeFields.bind(this);
+  }
+  handleChangeFields = (e1) => {
+    this.setState({
+      ...this.state,
+      [e1.target.name]: e1.target.value,
+    });
+    console.log(this.state, "formField");
+  };
+  handleLogin = (e) => {
+    const shopid = this.state.shopid;
+    const model = this.state.model;
+
+    const data = {
+      shopid,
+      model,
+    };
+    axios
+      .get("http://localhost/project/generate_qr.php", { params: data })
+      .then((kalu) => {
+        console.log(kalu);
+        console.log(kalu.data[0]);
+        this.setState({
+          result: kalu.data[0],
+        });
+        this.setState({
+          test: JSON.stringify([
+            this.state.result,
+            this.state.wdate,
+            this.state.pdate,
+          ]),
+        });
+        console.log(this.state.test, "Lalu");
+        console.log(this.state.result, "Kalu");
+      });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="main">
+          <div className="card">
+            <div className="inner-box">
+              <div>
+                <h2>
+                  <u>Generate New QR</u>
+                </h2>
                 <input
                   type="text"
                   className="input-box"
                   placeholder="Product ID"
+                  name="model"
+                  id="model"
+                  onChange={this.handleChangeFields}
                   required
                 />
                 <input
                   type="text"
                   className="input-box"
                   placeholder="Shopkeeper ID"
+                  name="shopid"
+                  id="shopid"
+                  onChange={this.handleChangeFields}
                   required
                 />
                 <label className="lb">Purchase Date</label>
-                <input type="date" className="input-box" required />
+                <input
+                  type="date"
+                  className="input-box"
+                  name="pdate"
+                  id="pdate"
+                  onChange={this.handleChangeFields}
+                  required
+                />
                 <label className="lb">Warranty Till</label>
-                <input type="date" className="input-box" required />
-                <button type="submit" className="submit-btn">
-                  Submit
-                </button>
-              </form>
+                <input
+                  type="date"
+                  className="input-box"
+                  name="wdate"
+                  id="wdate"
+                  onChange={this.handleChangeFields}
+                  required
+                />
+                <input
+                  type="submit"
+                  value="Generate"
+                  className="submit-btn"
+                  onClick={this.handleLogin}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <br></br>
+        </div>
+        <div className="main">
+          <div className="card">
+            <div className="inner-box">
+              <div>
+                <QRCode value={this.state.test} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default Generate_New_QR;
--- src/Shopkeeper.js
+++ src/Shopkeeper.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-pascal-case */
 import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Generate_New_QR } from "./S_components/Generate_New_QR";
+import Generate_New_QR from "./S_components/Generate_New_QR";
 import { Navbar } from "./S_components/Navbar";
 import { Products_List } from "./S_components/Products_List";
 class Shopkeeper extends Component {
